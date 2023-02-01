f57756d942792ae633b377a9e9c6ce0241b43a60
pinrongc <pinrongc156@gmail.com>
2023-01-31 22:29:27 -0500

    .

--- api-project/main.js
+++ api-project/main.js
@@ -1,11 +1,13 @@
-
-const Gallery = " https://pokeapi.co/api/v2/pokemon/"
+/* 
+const Gallery = " https://pokeapi.co/api/v2/pokemon/";
 
 async function getData(url) {
 try{
   const response = await fetch(url);
 
-  if(response.status <200 || response.status >299){
+  if(response.status <200 || response.status >299)
+  
+  {
     console.log(response.status);
     throw error(response);
   }else {
@@ -16,7 +18,7 @@ try{
 
     function displayPokemon(){
       pokemonArray.forEach((pokemon) => {
-        DOMselectors.display.insertAdjacentHTML("afterbegin", <div id= "pokemon"> <h2> ${pokemon.name} </h2></div>);
+        DOMselectors.display.insertAdjacentHTML("afterbegin", <div class= "pokemon"> <h2> ${pokemon.name} </h2></div>);
       });
     }
 
@@ -31,7 +33,13 @@ catch(error){
 }
   getData(Gallery);
 
+  const DOMselectors = {
+    pokemon: document.getElementById("pokemon"),
+    display: document.getElementById("display"),
+    form: document.getElementById("form")
+  };   */
 
+/* 
 document.getElementById("SearchBar").onkeyup = function searchFunction(){
   const input = document.getElementById("SearchBar").value.toUpperCase();
   const pokemon = 
@@ -39,24 +47,92 @@ document.getElementById("SearchBar").onkeyup = function searchFunction(){
   const pokemonContent = document.getElementsByTagName("h2");
 }  
 
+for (let i = 0; i < pokemonContent.length; i++) {
+  let pokeName = 
+  pokemon[i].getElementsByTagName(h2)[0];
+  if (pokeName) {
+    let textValue = pokeName.textContent
+    pokeName.innerHTML;
+    if (textValue.toUpperCase().indexOf(input)> -1){
+      pokemon[i].style.display ="";
+    } 
+    else{
+      pokemon [i].style.display = "none";
+    }
+  }
+}; */
 
+// DOMselectors.form.addEventListener("submit", function(event){
+//   event.preventDefault();
+// });
 
-const DOMselectors = {
-  pokemon: document.getElementById("pokemon"),
-  display: document.getElementById("display"),
-  form: document.getElementById("form")
-}; 
-
-
-DOMselectors.form.addEventListener("submit", function(event){
-  event.preventDefault();
-});
-
-console.log(DOMselectors)
+// console.log(DOMselectors)
 
 
 //DOMselectors.pokemon.addEventListener("click", getData(Gallery))
 
+import "./style.css"
 
+const Gallery = "https://pokeapi.co/api/v2/pokemon/";
 
+const DOMselectors = {
+  pokemon: document.getElementById("pokemon"),
+  display: document.getElementById("display"),
+  form: document.getElementById("form"),
+};
+
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+
+    if (response.status < 200 || response.status > 299) {
+      console.log(response.status);
+      throw error(response);
+    } else {
+      const data = await response.json();
+      const pokemonArray = data.results;
+      console.log(pokemonArray);
+      function displayPokemon() {
+        pokemonArray.forEach((pokemon) => {
+          DOMselectors.display.insertAdjacentHTML(
+            "afterbegin",
+            `<div class="pokemon"> 
+            <h2> ${pokemon.name} </h2> 
+            </div>`
+          );
+        });
+      }
+      displayPokemon();
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("sad");
+  }
+}
+
+getData(Gallery);
+
+document.getElementById("search").onkeyup = function searchFunction(){
+  const input = document.getElementById("search").value.toUpperCase();
+  const pokemon = document.querySelectorAll(".pokemon");
+  const pokemonContent = document.getElementsByTagName("h2");
+
+  for (let i = 0; i <pokemonContent.length; i++) {
+    let pokeName = pokemon[i].getElementsByTagName(`h2`)[0];
+    if (pokeName){
+      let textValue = pokeName.textContent; 
+     
+      if (textValue.toUpperCase().indexOf(input)> -1) {
+        pokemon[i].style.display = "";
+      } else {
+        pokemon[i].style.display = "none";
+      }
+    }
+  }
+};
+
+DOMselectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+console.log(DOMselectors);
 
