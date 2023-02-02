2b815aa49a0dea5cac202db12576d4913b8242c7
pinrongc <pinrongc156@gmail.com>
2023-02-01 22:22:15 -0500

    .

--- api-project/main.js
+++ api-project/main.js
@@ -1,78 +1,60 @@
-import "./style.css"
+import "./style.css";
 
-const Gallery = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+//const Gallery = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+//const poke = "https://pokeapi.co/api/v2/pokemon/"
 
 async function getData(url) {
-  try {
+ try {
     const response = await fetch(url);
-
     if (response.status < 200 || response.status > 299) {
-      console.log(response.status);
+    /*   console.log(response.status); */
       throw error(response);
     } else {
       const data = await response.json();
-/* 
-DOMselectors.box.insertAdjacentHTML{
+
+DOMselectors.box.insertAdjacentHTML(
   "afterbegin",
-  `<div class="poke"> 
+` <div class="poke"> 
 <h2 class="name">${data.name}</h2>
+<img src="${data.sprites.front_default}" class = img>
+<h3 class = "list">Abilities</h3>
+<ul class = "abilities"></ul>
 </div>`
-}
- */
-/* data.abilities.forEach(() => 
-document.querySelector(".abilities").insertAdjacentHTML("afterbegin",`<li class="list-item">${.abilities.name}</li>`
-)); */
-      const pokemonArray = data.results;
-      console.log(pokemonArray);
-      function displayPokemon() {
-        pokemonArray.forEach((pokemon) => {
-          DOMselectors.display.insertAdjacentHTML(
-            "afterbegin",
-            `<div class="pokemon"> 
-            <h2> ${pokemon.name} </h2> 
-            </div>`
-          );
-        });
-      }
-      displayPokemon();
-    }
-  } catch (error) {
+);
+
+data.abilities.forEach((pokemon) => 
+document
+.querySelector(".abilities").insertAdjacentHTML("afterbegin",
+`<li class="list">${pokemon.ability.name}</li>`
+)
+); 
+  }
+
+  } 
+  catch (error) {
     console.log(error);
     console.log("sad");
-  }
+ }
 }
 
-getData(Gallery);
+ function clear() {
+  DOMselectors.name.value = "";
+  DOMselectors.box.innerHTML = "";
+ } 
 
 const DOMselectors = {
-  pokemon: document.getElementById("pokemon"),
-  display: document.getElementById("display"),
-  form: document.getElementById("form"),
-};
+form: document.getElementById("form"),
+name: document.getElementById("pokename"),
+box: document.getElementById("box"),
+click: document.getElementById("submit")
+}
+console.log(DOMselectors);
 
-
-
-document.getElementById("search").onkeyup = function searchFunction(){
-  const input = document.getElementById("search").value.toUpperCase();
-  const pokemon = document.querySelectorAll(".pokemon");
-  const pokemonContent = document.getElementsByTagName("h2");
-
-  for (let i = 0; i <pokemonContent.length; i++) {
-    let pokeName = pokemon[i].getElementsByTagName(`h2`)[0];
-    if (pokeName){
-      let text = pokeName.textContent; 
-     
-      if (text.toUpperCase().indexOf(input)> -1) {
-        pokemon[i].style.display = "";
-      } else {
-        pokemon[i].style.display = "none";
-      }
-    }
-  }
-};
 
 DOMselectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
-});
-console.log(DOMselectors);
-
+  let pokemon = DOMselectors.name.value;
+  const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+  getData(URL);
+  clear();
+}); 
